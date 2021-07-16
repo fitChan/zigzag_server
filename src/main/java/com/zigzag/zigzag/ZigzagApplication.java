6473da8a35d91dd3ba48d13763a3533ad274c3c7
fitChan <84306157+fitChan@users.noreply.github.com>
2021-07-16 19:05:27 +0900

    test

---
+++ src/main/java/com/zigzag/zigzag/ZigzagApplication.java
@@ -0,0 +1,13 @@
+package com.zigzag.zigzag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigzagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZigzagApplication.class, args);
+    }
+
+}
